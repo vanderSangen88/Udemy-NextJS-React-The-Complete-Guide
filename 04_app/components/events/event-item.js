edd7e429e0df343a69700ba5bb7a53f8213d22b3
vanderSangen88 <nick.vandersangen@gmail.com>
2022-02-04 16:51:23 +0100

    progress S4 L74 - 75

--- 04_app/components/events/event-item.js
+++ 04_app/components/events/event-item.js
@@ -1,4 +1,8 @@
-import Link from "next/link";
+import Button from "../ui/button";
+import styles from "./event-item.module.css";
+import DateIcon from "./../icons/date-icon";
+import AddressIcon from "./../icons/address-icon";
+import ArrowRightIcon from "./../icons/arrow-right-icon";
 
 const EventItem = ({ title, image, date, location, id }) => {
   const humanReadableDate = new Date(date).toLocaleDateString("nl-NL", {
@@ -9,18 +13,27 @@ const EventItem = ({ title, image, date, location, id }) => {
   const formattedAddress = location.replace(", ", "\n");
   const exploreLink = `/events/${id}`;
   return (
-    <li>
+    <li className={styles.item}>
       <img src={`/${image}`} alt={title} />
-      <div>
-        <div>
+      <div className={styles.content}>
+        <div className={styles.summary}>
           <h2>{title}</h2>
-          <time>{humanReadableDate}</time>
-          <div>
+          <div className={styles.date}>
+            <DateIcon />
+            <time>{humanReadableDate}</time>
+          </div>
+          <div className={styles.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
-          <Link href={exploreLink}>Explore Event</Link>
+        <div className={styles.actions}>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={styles.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
